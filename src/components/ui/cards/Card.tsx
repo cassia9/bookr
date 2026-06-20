234dd13468c938d1a34ee4c7b68ab5371c12ee8b
Cassia <CL@kakudeAir.home>
2026-06-20 18:25:13 +0800

    修正 Card 元件型別定義，解決 Cloudflare 部署 TS 錯誤

--- src/components/ui/cards/Card.tsx
+++ src/components/ui/cards/Card.tsx
@@ -1,33 +1,37 @@
-import { forwardRef, HTMLAttributes, ReactNode } from 'react'
+import { forwardRef, HTMLAttributes } from 'react'
 import { cn } from '@/lib/cn'
 
-const Card = forwardRef((props, ref) => {
-  return (
-    <div ref={ref} {...props}>
-      {props.children}
-    </div>
+const Card = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={cn('bg-white rounded-2xl border border-slate-200 shadow-sm', className)}
+      {...props}
+    />
   )
-})
-
+)
 Card.displayName = 'Card'
 
-export const CardHeader = forwardRef((props, ref) => {
-  return <div ref={ref} {...props} />
-})
-
+export const CardHeader = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn('px-6 py-4 border-b border-slate-100', className)} {...props} />
+  )
+)
 CardHeader.displayName = 'CardHeader'
 
-export const CardBody = forwardRef((props, ref) => {
-  return <div ref={ref} {...props} />
-})
-
+export const CardBody = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn('px-6 py-4', className)} {...props} />
+  )
+)
 CardBody.displayName = 'CardBody'
 
-export const CardFooter = forwardRef((props, ref) => {
-  return <div ref={ref} {...props} />
-})
-
+export const CardFooter = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn('px-6 py-4 border-t border-slate-100', className)} {...props} />
+  )
+)
 CardFooter.displayName = 'CardFooter'
 
-export { Card, CardHeader, CardBody, CardFooter }
+export { Card }
 export default Card
